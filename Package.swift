5d768afd02d44924a69ef6dfdcae36bb1f7a0705
Tieme van Veen <tiemevanveen@hotmail.com>
2019-11-30 23:16:25 +0100

    Adding older Neo4j and Bolt

--- Package.swift
+++ Package.swift
@@ -10,11 +10,14 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
+        .package(url: "https://github.com/teameh/Neo4j-swift.git", .branch("4.0.2-revived")),
+        .package(url: "https://github.com/teameh/Bolt-swift.git", .branch("1.0.2-swift-tools-4")),
+
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Theo", "Bolt"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
